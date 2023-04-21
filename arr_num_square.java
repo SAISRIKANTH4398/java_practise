84ef77939a89808c856b34681bb75766128c39af
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-21 11:08:18 +0530

    Array Number squaring using Java

---
+++ arr_num_square.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class arr_num_square {
+    public static void main(String[] args){
+        Scanner num = new Scanner(System.in);
+        int size = num.nextInt();
+        int[] num_seq = new int[size];
+        int index = 0;
+        while (index<size){
+           num_seq[index] = num.nextInt();
+           index = index + 1;
+        }
+        for(int eachItem:num_seq){
+            System.out.print(eachItem+" ");
+        }
+        num.close();
+    }
+}
