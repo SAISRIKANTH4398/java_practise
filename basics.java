4cf6cbc351e995368d81e63ef36a27948cfe63ad
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-21 14:44:03 +0530

    Update basics code

--- basics.java
+++ basics.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 class IoBasics {
     public static void main(String[] args){
         Scanner sc_str = new Scanner(System.in); //scanning input
-        //Scanner sc_int = new Scanner(System.in);
-        //int num = sc_int.nextInt();  //reading an Int value
+        int num = sc_str.nextInt();  //reading an Int value
         String person = sc_str.next();  // reads string value that is after first white spaces
         String name = sc_str.nextLine(); // reads string values
-        //System.out.println(num);
+        System.out.println(num);
         System.out.println(name);
         System.out.println(person);
     }
