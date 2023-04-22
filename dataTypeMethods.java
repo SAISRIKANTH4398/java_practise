80314c89660490d686196d0b158005052abae872
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-22 16:07:10 +0530

    Data Type Conversion Methods

--- dataTypeMethods.java
+++ dataTypeMethods.java
@@ -20,7 +20,39 @@ class explicitTypeCasting {
     public static void main(String[] args){
         long value = 10L;
         float num = (float) value;
+        char x = 'A';
         System.out.println(((Object)num).getClass().getSimpleName());
+        System.out.println((int) x);  //It returns the ASCII values
     
+    }
+}
+
+class dataChangeMethods{
+    public static void main(String[] args){
+        int num = 2;
+        float floatNum = 2.5f;
+        char a = 'i';
+        System.out.println(String.valueOf(num));  //Converts to String from primitive datatype
+        System.out.println(String.valueOf(a));
+        System.out.println(String.valueOf(floatNum));
+    }
+}
+
+class dataTypeChangeMethods{
+    public static void main(String[] args){
+        String value = "123";
+        String doubleNum = "12.345";
+        char ch = '3';
+        int number = 3;
+        int num = Integer.parseInt(value);   //Converts Str to int value
+        int num1 = Character.getNumericValue(ch);  // Converts Character to Integer value
+        char ch1 = Character.forDigit(number, 10);  //Converts Integer to Character
+        double dbl = Double.parseDouble(doubleNum);
+        float floatNum = Float.parseFloat(doubleNum);
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(ch1);
+        System.out.println(dbl);
+        System.out.println(floatNum);
     }
 }
