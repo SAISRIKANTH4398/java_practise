2902f6257e5cef817146cc43e7c2a895bf1c960f
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-26 07:24:38 +0530

    Smallest Among 3 Numbers

---
+++ smallestAmong3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class SmallestAmong3{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(a<b?(a<c?a:c):(b<c?b:c));
+        sc.close();
+    }
+}
