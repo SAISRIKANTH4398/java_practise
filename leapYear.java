4a2c6fb4ea53f6edf442aeec736638fed34d1d9f
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-27 13:05:51 +0530

    Leap Year

---
+++ leapYear.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+        int Y = sc.nextInt();
+        if(Y%100==0){
+            if(Y%400==0){
+                System.out.println("True");
+            }else{
+                System.out.println("False");
+            }
+        }else if(Y%4==0){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+        sc.close();
+    }
+}
