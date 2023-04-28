d26121c0f54d32d0d90255847c22b1a972c14219
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-28 15:56:02 +0530

    Electricity Bill

--- electricityBill.java
+++ electricityBill.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main1{
+class electricityBill{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
