ec0a509783e1c9661a2039628d4800d75f51d0c3
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-28 16:00:52 +0530

    While, do-while and for loops

--- loops.java
+++ loops.java
@@ -10,3 +10,27 @@ class whileLoop {
         }
     }
 }
+
+class doWhile {
+    public static void main(String[] args) {
+        int n = 4;
+        int counter = 0;
+
+        do {       //It executes atleast one time
+            n = n + 1;  //First executes and then checks the condition
+            System.out.println(n);
+            counter = counter + 1;
+        } while (counter < 3);
+    }
+}
+
+class forLoop {
+    public static void main(String[] args) {
+        int num = 4;
+
+        for (int counter = 0; counter < 3; counter = counter + 1 ) {
+            num = num + 1;
+            System.out.println(num);
+        }
+    }
+}
