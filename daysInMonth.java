8b065c75486cfd73bffaccdc1663e3a9a7fbf995
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-28 14:14:45 +0530

    Days in a Month using Switch Case

---
+++ daysInMonth.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        switch(input){
+            case "February":
+                System.out.println("28");
+                break;
+            case "April":
+                System.out.println("30");
+                break;
+            case "June":
+                System.out.println("30");
+                break;
+            case "September":
+                System.out.println("30");
+                break;
+            case "November":
+                System.out.println("30");
+                break;
+            default:
+                System.out.println("31");
+                break;
+        }
+        sc.close();
+    }
+}
