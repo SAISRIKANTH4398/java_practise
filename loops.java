6fcce8a98256d196e8eaa225d1f2919b430fa239
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-28 15:50:48 +0530

    Loops

---
+++ loops.java
@@ -0,0 +1,12 @@
+class whileLoop {
+    public static void main(String[] args) {
+        int num = 4;
+        int counter = 0;
+
+        while (counter < 3) {
+            num = num + 1;
+            System.out.println(num);
+            counter = counter + 1;
+        }
+    }
+}
