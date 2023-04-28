261c80b7de53e3de210ad25b5f25082eb87bd121
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-28 14:13:03 +0530

    Denominations of 100s, 50s, 10s and ones

---
+++ denominations.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int hundreds = N/100;
+        N = N%100;
+        int fifties = N/50;
+        N = N%50;
+        int tens = N/10;
+        N = N%10;
+        int ones = N;
+        System.out.println("100:"+hundreds);
+        System.out.println("50:"+fifties);
+        System.out.println("10:"+tens);
+        System.out.println("1:"+ones);
+        sc.close();
+    }
+}
