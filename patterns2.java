64eb781fb880161a5ad7819758cd35d45780cfc1
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-04-30 17:27:18 +0530

    Patterns 2

--- patterns2.java
+++ patterns2.java
@@ -14,3 +14,51 @@ class hollowSquare{
         sc.close();
     }
 }
+
+
+class hollowRightTriangle{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=0; i<N; i++){
+            if(i==0){
+                System.out.println("* ".repeat(N));
+            }else if(i==N-1){
+                System.out.println("  ".repeat(N-1)+"*");
+            }
+            else{
+                System.out.println("  ".repeat(i) + "* "+"  ".repeat(N-i-2)+"*");
+            }
+        }
+        sc.close();
+    }
+}
+
+class hollowRightTriangle2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=0; i<N; i++){
+            if(i==N-1){
+                System.out.println("* ".repeat(N));
+            }else if(i==0){
+                System.out.println("  ".repeat(N-1)+"*");
+            }
+            else{
+                System.out.println("  ".repeat(N-i-1) + "* "+"  ".repeat(i-1)+"*");
+            }
+        }
+        sc.close();
+    }
+}
+
+class RightTriangle{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=0; i<N; i++){
+            System.out.println("  ".repeat(N-i-1)+"* ".repeat(i+1));
+        }
+        sc.close();
+    }
+}
