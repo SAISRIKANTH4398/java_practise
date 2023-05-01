a513f8a94a13f266befabdabbd5fbf6ee1d345cd
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-01 11:22:28 +0530

    Patterns 3

--- patterns3.java
+++ patterns3.java
@@ -55,3 +55,17 @@ class solidDiamond{
         sc.close();
     }
 }
+
+class halfPyramid{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
