c666c3ac48f1386e277cd246cb4ef7d6362e8725
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-02 16:40:19 +0530

    GCD and LCM using Java

--- gcd&lcm.java
+++ gcd&lcm.java
@@ -16,3 +16,19 @@ class GCD{
         sc.close();
     }
 }
+
+class lcm{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int greater = a>b?a:b;
+        for(int i=greater; i<=a*b; i++){
+            if(i%a==0 && i%b==0){
+                System.out.println(i);
+                break;
+            }
+        }
+        sc.close();
+    }
+}
