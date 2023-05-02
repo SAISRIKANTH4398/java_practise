593064b233173138c57ee8dbd56e8316db416712
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-02 16:47:26 +0530

    Patterns part 4

--- patterns4.java
+++ patterns4.java
@@ -73,3 +73,14 @@ class fullPyramid{
         sc.close();
     }
 }
+
+class invertedHalfPyramid{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1; i<=N ; i++){
+            System.out.println("* ".repeat(N-i+1));
+        }
+        sc.close();
+    }
+}
