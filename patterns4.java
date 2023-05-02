ee75597302179c42dae03b0fb1c6a204a7e7e6f2
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-02 16:46:07 +0530

    Patterns 4

--- patterns4.java
+++ patterns4.java
@@ -57,3 +57,19 @@ class halfPyramid4{
         sc.close();
     }
 }
+
+
+class fullPyramid{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int N=sc.nextInt();
+        for(int i=1; i<=N;i++){
+            System.out.print(" ".repeat(N-i));
+            for(int j=1; j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
