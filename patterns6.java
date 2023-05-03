f0dc54aade04b3f92d6260d02879212459b6d458
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-03 21:27:23 +0530

    Patterns 6

--- patterns6.java
+++ patterns6.java
@@ -41,3 +41,15 @@ class invertedPyramid{
         sc.close();
     }
 }
+
+
+class invertedTriangle{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++){
+            System.out.println(" ".repeat(i-1)+"* ".repeat(N-i+1));
+        }
+        sc.close();
+    }
+}
