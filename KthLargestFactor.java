c3c275b3d52f8c49c5fa6bcf280e1214fb909d25
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-05 23:03:18 +0530

    Kth Largest Factor

--- KthLargestFactor.java
+++ KthLargestFactor.java
@@ -16,3 +16,30 @@ class largestFactor{
         sc.close();
     }
 }
+
+
+
+class kthlargestFactor{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int k = sc.nextInt();
+        int count = 0;
+        int output = 0;
+        for(int i=N; i>=1; i-- ){
+            if(N%i==0){
+                count++;
+                if(count==k){
+                    output = i;
+                    break;
+                }
+            }
+        }
+        if(output==0){
+            System.out.println(1);
+        }else{
+            System.out.println(output);
+        }
+        sc.close();
+    }
+}
