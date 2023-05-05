b7a0a1a71511eeaf671fbfa29087d9cb1b7a5b84
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-05 22:58:46 +0530

    Pattern 7

--- patterns7.java
+++ patterns7.java
@@ -104,3 +104,15 @@ class digit9{
         sc.close();
     }
 }
+
+
+class pyramid{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++){
+            System.out.println(". ".repeat(N-i)+"0 ".repeat(2*i-1)+". ".repeat(N-i));
+        }
+        sc.close();
+    }
+}
