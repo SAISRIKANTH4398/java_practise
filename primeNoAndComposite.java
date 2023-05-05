95b505878b618e147827bf2830984e7a0ef4c156
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-05 23:05:04 +0530

    Prime and Composite Numbers

--- primeNoAndComposite.java
+++ primeNoAndComposite.java
@@ -58,3 +58,24 @@ class PrimeNumbers{
         sc.close();
     }
 }
+
+
+class compositeNum{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        for(int i=M; i<=N; i++){
+            int count=0;
+            for(int j=2;j<=i; j++){
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count>1){
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+}
