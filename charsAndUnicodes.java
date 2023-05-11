65bc8558a0fea3b92e80be8681ed319e2f33b5c4
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-11 10:39:45 +0530

    Char and Unicode Values

--- charsAndUnicodes.java
+++ charsAndUnicodes.java
@@ -1,7 +1,9 @@
 class Unicodes {
     public static void main(String[] args){
         char ch = 'A';
+        char ch1 = 'B';
         int unicode = ch;           //Implicit conversion
         System.out.println(unicode);
+        System.out.println((int) ch1);     //Explicit conversion
     }
 }
