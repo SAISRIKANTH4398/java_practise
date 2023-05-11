5363f17ee7fcbd65c8491ac800973e2c6a30e726
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-11 10:41:51 +0530

    Char and Unicode Values

--- charsAndUnicodes.java
+++ charsAndUnicodes.java
@@ -7,3 +7,12 @@ class Unicodes {
         System.out.println((int) ch1);     //Explicit conversion
     }
 }
+
+
+class Alphabets{
+    public static void main(String[] args){
+        for (int unicodeValue = 65; unicodeValue < 91; unicodeValue = unicodeValue + 1) {
+            System.out.println((char)unicodeValue);    //Printing alphabet using unicode values
+        }
+    }
+}
