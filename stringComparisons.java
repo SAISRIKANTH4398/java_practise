b49a69f3beaca360c72884c2349b37c5548c4033
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-12 23:40:46 +0530

    String Comparing Methods

--- stringComparisons.java
+++ stringComparisons.java
@@ -7,8 +7,9 @@ class stringComparisons {
     public static void main(String[] args){
         String a = new String("ABC");
         String b = new String("ABC");
+        String c = new String("abc");
         System.out.println(a.equals(b)); //returns true
         System.out.println(a==b);  //returns false because memory is different for 2 variables
-
+        System.out.println(a.equalsIgnoreCase(c)); //returns true because it ignores case
     }
 }
