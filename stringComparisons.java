e7dedb54b6a65daed5f06fb604f84eef25460eca
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-12 23:47:04 +0530

    String Comparision Methods

--- stringComparisons.java
+++ stringComparisons.java
@@ -8,8 +8,16 @@ class stringComparisons {
         String a = new String("ABC");
         String b = new String("ABC");
         String c = new String("abc");
+        String d = "A";
+        String e = "B";
         System.out.println(a.equals(b)); //returns true
         System.out.println(a==b);  //returns false because memory is different for 2 variables
         System.out.println(a.equalsIgnoreCase(c)); //returns true because it ignores case
+        System.out.println(d.compareTo(e));  
+        /*Compare to operator compares the unicode values of 2 strings. 
+        Returns 1 if str1>str2, 
+        0 if str1==str2
+        -1 if str1<str2 */
+        
     }
 }
