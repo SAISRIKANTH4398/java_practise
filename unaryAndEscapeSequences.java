b87d87ae3662d20d017ea7e8f474affb584dbeb6
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-24 10:04:53 +0530

    Unary operator and Escape Sequence

--- unaryAndEscapeSequences.java
+++ unaryAndEscapeSequences.java
@@ -2,15 +2,19 @@
  * 1.Increment Operator
  * 2.Decrement operator
  * 
- * 1.1 Prefix: ++x Variable's value incremented first and then updated value is used.
+ * 1.1/2.1 Prefix: ++x Variable's value incremented/decremented first and then updated value is used.
  */
 
 class prefix{
     public static void main(String[] args){
         int a = 10;
+        int b = 20;
         ++a;               //a=11
+        --b;
         int number = ++a;   //a=12, number=12
+        int num = --b;
         System.out.println(a);
         System.out.println(number);
+        System.out.println(num);
     }
 }
