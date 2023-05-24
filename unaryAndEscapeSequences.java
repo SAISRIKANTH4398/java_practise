f4998dd261e4fd9a1f256843292428db0957a665
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-05-24 10:12:20 +0530

    Unary Operators ans Escape Seq

--- unaryAndEscapeSequences.java
+++ unaryAndEscapeSequences.java
@@ -2,7 +2,7 @@
  * 1.Increment Operator
  * 2.Decrement operator
  * 
- * 1.1/2.1 Prefix: ++x Variable's value incremented/decremented first and then updated value is used.
+ * 1.1/2.1 Prefix: ++x/--x Variable's value incremented/decremented first and then updated value is used.
  */
 
 class prefix{
@@ -10,11 +10,32 @@ class prefix{
         int a = 10;
         int b = 20;
         ++a;               //a=11
-        --b;
+        --b;               //b=19
         int number = ++a;   //a=12, number=12
-        int num = --b;
+        int num = --b;      //num=18, b=18
         System.out.println(a);
         System.out.println(number);
         System.out.println(num);
+        System.out.println(b);
+    }
+}
+
+
+/*1.2/2.2 Postfix: x++/x--:  in Postfix, variable's value is first used for computing the result then 
+incremented/decremented  */
+
+
+class postfix{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        a++;               //a=11
+        b--;               //b=19
+        int number = a++;   //a=12, number=11
+        int num = b--;     //b=18, num =19
+        System.out.println(a);
+        System.out.println(number);
+        System.out.println(num);
+        System.out.println(b);
     }
 }
