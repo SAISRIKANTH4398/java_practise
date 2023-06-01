6c1db9c670b50b472ea2bf4de5fa84c5209a3a31
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-06-01 11:11:54 +0530

    Big Integers

---
+++ bigIntegers.java
@@ -0,0 +1,9 @@
+/*In general, integers and long can store numbers in the range (-2^31-1 to 2^31-1) and (-2^63-1 to 2^63-1) respectively.
+ * But In real world sceneraios, we may have to store numbers larger than these ranges.
+ * Hence we use Big Integers.
+ */
+
+
+public class bigIntegers {
+    
+}
