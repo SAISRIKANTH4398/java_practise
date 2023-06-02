a7cc8e83b85b9dce7ef38eb231fc3d400d6f4ade
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-06-02 16:34:17 +0530

    Big Integers

--- bigIntegers.java
+++ bigIntegers.java
@@ -3,7 +3,14 @@
  * Hence we use Big Integers.
  */
 
+/*Big Int is a class which involve very big mathematical operations that are not in the limit of all
+ primitive datatypes */
+ import java.math.BigInteger;
 
 public class bigIntegers {
-    
+   public static void main(String[] args){
+    String str = "35593696540923939295459856";
+    BigInteger BigNum = new BigInteger(str);  //Declaring and initializing Big Integers
+    System.out.println(BigNum);
+   }
 }
